ab0bee6e29f35ed23949f2a7e45b7316cc26c73e
Neda Gholami <90248679+neda1998@users.noreply.github.com>
2023-07-03 14:22:17 +0330

    add taiwind css, strocture folder, add header to project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Logo from '../Logo/Logo'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+          <Logo />
+        </div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Header
