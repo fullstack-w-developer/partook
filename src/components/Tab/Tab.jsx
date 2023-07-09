4890b37e21ea7ea2901c05531f42e955b4eb5e98
Neda Gholami <90248679+neda1998@users.noreply.github.com>
2023-07-09 11:57:13 +0330

    add style to donait page

---
+++ src/components/Tab/Tab.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { PiInstagramLogoFill } from "react-icons/pi";
+import { FaTelegramPlane } from "react-icons/fa";
+
+const Tab = () => {
+    const [tab, setTab] = useState(0);
+    return (
+        <div>
+            <button onClick={() => setTab(tab === 0)}>پروفایل</button>
+            <button onClick={() => setTab(tab === 1)}>مقاله ها</button>
+            {/* profile */}
+            <div>
+                <div>
+                    <p>
+                        به عزیزات یه کتاب هدیه بده
+                    </p>
+                    {/* counter */}
+                    <span>نام پرداخت کننده(اختیاری)</span>
+                    <input type="textarea" />
+                    <span>متن پیام(اختیاری)</span>
+                    <input type="textarea" />
+                </div>
+                <div className='aboutusProf'>
+                    <p>درباره ما</p>
+                    <span>
+                        پرتوک وبسایتی که خرید کتاب رو براتون اسون میکنه.
+                    </span>
+                    <p>لینک صفحات کافیته در دیگر سایت ها</p>
+                    <div className='flexAlign'>
+                        <div className='linkSocialmedia flexAlign'>
+                            <FaTwitter size={24} className='socialIcon' />
+                            <span>توییتر</span>
+                        </div>
+                        <div className='linkSocialmedia flexAlign'>
+                            <FaLinkedinIn size={24} className='socialIcon' />
+                            <span>لینکدین</span>
+                        </div>
+                        <div className='linkSocialmedia flexAlign'>
+                            <PiInstagramLogoFill size={24} className='socialIcon' />
+                            <span>اینستاگرام</span>
+                        </div>
+                        <div className='linkSocialmedia flexAlign'>
+                            <FaTelegramPlane />
+                            <span>تلگرام</span>
+                        </div>
+                    </div>
+                </div>
+                {/* users */}
+                {/* users */}
+            </div>
+            {/* profile */}
+        </div>
+    )
+}
+
+export default Tab
--- src/pages/Donait.jsx
+++ src/pages/Donait.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Donait = () => {
   return (
-    <div>Donait</div>
+    <div>
+      
+    </div>
   )
 }
 
