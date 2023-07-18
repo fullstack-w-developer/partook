252aa7309be55c664532e83bfb5f7c2cdc135370
Neda Gholami <90248679+neda1998@users.noreply.github.com>
2023-07-18 12:26:31 +0330

    add cardBox compponent and styles to that

--- src/components/Dashboard/CardBox.jsx
+++ src/components/Dashboard/CardBox.jsx
@@ -1,13 +1,50 @@
 import React from 'react';
+import { AiFillHeart } from "react-icons/ai";
+import "../../styles/Dashboard.css";
 
 const cards = [
-    {headCard:"", count:0, amount:""}
+  {
+    id: 1, headCard: "ساعت", count: 0, amount: "1402/02/02", borderCount: <AiFillHeart />,colorCount:"red",
+  },
+  {
+    id: 2, headCard: "امار کتاب های من", count: 0, amount: "مبلغ کتاب ها(تومان)", borderCount: "0",colorCount:"red",
+  },
+  { id: 3, headCard: "کتاب های امروز من", count: 0, amount: "مبلغ کتاب ها(تومان)", borderCount: "0",colorCount:"red", },
+  { id: 4, headCard: "برداشت های من", count: 0, amount: "مبلغ کتاب ها(تومان)", borderCount: "0",colorCount:"red", },
+  {
+    id: 5, headCard: "کیف پول من", count: 0, amount: "مبلغ(تومان)", borderCount: "Wallet",colorCount:"red",
+  },
+  {
+    id: 6, headCard: "لایک های من", count: 0, amount: "نعداد لایک های من", borderCount: <AiFillHeart />,colorCount:"red",
+  }
 ]
 
 const CardBox = () => {
   return (
-    <div>
-        <p>{headCard}</p>
+    <div className='grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6 mt-12'>
+      {
+        cards.map(card => {
+          const { headCard, count, amount, borderCount, colorCount, id } = card
+          return (
+            <div className='cardBox' key={id}>
+              <h6>{headCard}</h6>
+              <div className='flexAlign cardAmount'>
+                <div>
+                  <p>
+                    {borderCount}
+                  </p>
+                </div>
+                <div className='flexCol countCard'>
+                  <span style={{color:colorCount}}>
+                    {count}
+                  </span>
+                  <p>{amount}</p>
+                </div>
+              </div>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import CardBoxNotif from './CardBoxNotif'
+import CardBox from './CardBox'
 
 const Dashboard = () => {
   return (
     <div>
         <CardBoxNotif />
+        <CardBox />
     </div>
   )
 }
